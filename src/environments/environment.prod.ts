62ab68e0583aaab3d06f3442c4b0a322f6a8e16c
Kode Sonik <k0d3.s0n1k@gmail.com>
2022-01-05 13:37:57 +0000

    set remote api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   hmr: false,
-  apiUrl: 'http://localhost:4000',
+  apiUrl: 'https://ks-stock-api.herokuapp.com/',
    firebase: {
     projectId: 'sonik-shop',
     appId: '1:261615791181:web:84b91c1187be52a9a64004',
